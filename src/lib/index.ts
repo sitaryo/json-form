8ca2a17e1ca12e3b9dc45b47e65f8d5fbc545f48
licky <895260021@qq.com>
2022-04-25 09:21:03 +0800

    调整 export ,发布新版本

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,8 +1,9 @@
-import InputFactory from "./InputFactory";
-import * as Type from "./type";
-import HtmlInput from "./inputs/HtmlInput";
-import * as InputInterface from "./inputs/input.interface";
+export * from "./type";
+export * from "./inputs/input.interface";
+
 import FieldInput from "./form/FieldInput";
 import JsonForm from "./form/JsonForm";
+import InputFactory, { initFactory } from "./InputFactory";
+import HtmlInput from "./inputs/HtmlInput";
 
-export { InputFactory, Type, HtmlInput, InputInterface, FieldInput, JsonForm };
+export { FieldInput, JsonForm, InputFactory, initFactory, HtmlInput };
